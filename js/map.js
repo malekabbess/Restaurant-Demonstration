4dee24950d1455c82048584614c815210f1282e4
malekabbess <malek.abbes@bearstudio.fr>
2023-06-16 15:42:17 +0100

    HTML/CSS/Bootstrap :rocket:

---
+++ js/map.js
@@ -0,0 +1,12 @@
+var map = L.map("mapid").setView([36.82231, 10.19238], 8);
+var marker = L.marker([31.82231, 10.19238]).addTo(map);
+marker
+  .bindPopup(
+    '<img src="img/logo.png" alt="logo" class="img_popup" /><h6 class="eden">Restauration - DEMO</h6><p>Rue La Tunisie </p> '
+  )
+  .openPopup();
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; Made by  <a href="https://www.malekabbes.com/">Malek abbes 😊</a>  ',
+}).addTo(map);
